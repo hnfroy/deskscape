9acde3c2af0e65ef5e761b90aef8cef7cb8b7253
HanifRoyyan <hnfroy@gmail.com>
2026-07-29 16:20:25 +0700

    Setup GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/deskscape",
+
+  assetPrefix: "/deskscape",
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
